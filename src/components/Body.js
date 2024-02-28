4fd89f9c4baa33ec5cd74c684d7b9701adcc97af
Satpal Singh <satpalsinghalive1@gmail.com.com>
2024-02-28 12:53:21 +0530

    shimmer UI added, mock data replaced with live swiggy api using useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,71 @@
 import Card from "./Card";
+import ShimmerCard from "./ShimmerCard";
 import DATA from "../utils/mockData";
+import { useState, useEffect } from 'react';
 
 const Body = () => {
-    const {restaurants} = DATA;
-    
-    return (
-        <div className='body'>
-            {/* <input type="text" /> */}
-            <div id="card-container">
-                {restaurants.map((restaurant)=>{
-                    return <Card restaurant={restaurant} key={restaurant.info.id}/>
-                })}
-                
+    // const {restaurants} = DATA;
+    const [restaurants, setRestaurants] = useState([]);
+
+
+    const fetchData = async () => {
+        const data = await
+            fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.5483694&lng=87.2813023&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+        console.log(restaurants);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    if (restaurants.length === 0) {
+        return (
+            <div className="body">
+                <div id="card-container">
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                    <ShimmerCard />
+                </div>
             </div>
-        </div>
-    )
+
+        )
+    }
+
+    else {
+        return (
+            <div className='body'>
+                <button id="btn-top-rated" onClick={
+                    () => {
+                        setRestaurants(
+                            restaurants.filter((restaurant) => {
+                                if (restaurant.info.avgRating > 4.5) {
+                                    return restaurants;
+                                }
+                            })
+                        )
+                        console.log(restaurants);
+                    }
+                }>Top rated</button>
+                <div id="card-container">
+                    {restaurants.map((restaurant) => {
+                        return <Card restaurant={restaurant} key={restaurant.info.id} />
+                    })}
+
+                </div>
+            </div>
+        )
+    }
+
+
 }
 
 export default Body;
---
+++ src/components/ShimmerCard.js
@@ -0,0 +1,20 @@
+
+const ShimmerCard = () => {
+    return (
+        <div id="shimmer-card-base" >
+            <div id="shimmer-card">
+                <div id="shimmer-food-image-div">
+                    
+                </div>
+                
+                <div id="shimmer-card-title">                    
+                </div>
+                <div id="shimmer-card-title">                    
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default ShimmerCard;
